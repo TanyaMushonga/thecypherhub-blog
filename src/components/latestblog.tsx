3fae6177aec2a47794401f763b0ff90a1417c6f5
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2024-12-21 21:39:24 +0200

    Add loading state to Latestblog component with Skeleton representation

--- src/components/latestblog.tsx
+++ src/components/latestblog.tsx
@@ -1,59 +1,67 @@
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { MdTimer } from "react-icons/md";
 import { LuPin } from "react-icons/lu";
 import { formatDate } from "@/lib/utils";
 import { BsDot } from "react-icons/bs";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import LatestSkeleton from "./latestSkeleton";
 
 function Latestblog() {
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
   return (
-    <div
-      className="flex md:flex-row flex-col gap-5 items-center pb-5"
-      onClick={() => {
-        router.push("/read/1");
-      }}
-    >
-      <Image
-        src="/blog_covers/codebuild.jpg"
-        width={600}
-        height={200}
-        alt="latest bog"
-        objectFit="cover"
-        className="rounded-md"
-      />
-      <div className="flex flex-col md:text-center gap-2">
-        <h1 className="text-white md:text-3xl font-bold text-xl">
-          How whatsapp was able to scale to 50 bilion messages a day
-        </h1>
-        <p className="text-white md:text-xl text-lg">
-          #1: learn more - whatsapp engineering concepts{" "}
-        </p>
-        <div className="flex flex-row items-center gap-2 md:justify-center">
-          {" "}
-          <MdTimer className="text-white" />{" "}
-          <p className="text-white">4 min read</p>
-        </div>
-        <div className="flex flex-row items-center md:justify-center">
-          <LuPin className="text-white mr-2" />{" "}
-          <p className="text-white">{formatDate(new Date(2024, 11, 2))}</p>
-          <div className="flex flex-row items-center md:justify-center">
-            <BsDot className="text-white" />
-            <Link
-              href={"https://tanya-mushonga.vercel.app/"}
-              className="text-white cursor-pointer hover:underline"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              TanyaMushonga
-            </Link>
+    <>
+      {loading ? (
+        <LatestSkeleton />
+      ) : (
+        <div
+          className="flex md:flex-row flex-col gap-5 items-center pb-5"
+          onClick={() => {
+            router.push("/read/1");
+          }}
+        >
+          <Image
+            src="/blog_covers/codebuild.jpg"
+            width={600}
+            height={200}
+            alt="latest bog"
+            objectFit="cover"
+            className="rounded-md"
+          />
+          <div className="flex flex-col md:text-center gap-2">
+            <h1 className="text-white md:text-3xl font-bold text-xl">
+              How whatsapp was able to scale to 50 bilion messages a day
+            </h1>
+            <p className="text-slate-300 md:text-xl text-lg">
+              #1: learn more - whatsapp engineering concepts{" "}
+            </p>
+            <div className="flex flex-row items-center gap-2 md:justify-center">
+              {" "}
+              <MdTimer className="text-slate-300" />{" "}
+              <p className="text-slate-300">4 min read</p>
+            </div>
+            <div className="flex flex-row items-center md:justify-center">
+              <LuPin className="text-slate-300 mr-2" />{" "}
+              <p className="text-slate-300">{formatDate(new Date(2024, 11, 2))}</p>
+              <div className="flex flex-row items-center md:justify-center">
+                <BsDot className="text-slate-300" />
+                <Link
+                  href={"https://tanya-mushonga.vercel.app/"}
+                  className="text-slate-300 cursor-pointer hover:underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  TanyaMushonga
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
