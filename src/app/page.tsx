042b360d7ca9131973ee11df7ecc35c91f5d9aff
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2024-12-21 21:38:54 +0200

    Add ArticleTabs component to the homepage for enhanced article navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
+import ArticleTabs from "@/components/articleTabs";
 import Latestblog from "@/components/latestblog";
 import Mostpopular from "@/components/mostpopular";
 
 export default function Home() {
   return (
     <div className="pt-10">
-      <div className="md:w-3/4 w-full mx-auto p-5 flex flex-col gap-5">
+      <div className="xl:w-3/4 w-full mx-auto p-5 flex flex-col gap-5">
         <Latestblog />
         <Mostpopular />
+        <ArticleTabs />
       </div>
     </div>
   );
