d2159c8dfd21380c0d9ad2a4dbf7e9db4f6546ce
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2024-12-22 22:34:38 +0200

    Add login page component for admin section

---
+++ src/app/(admin)/login/page.tsx
@@ -0,0 +1,12 @@
+import SignupFormDemo from "@/components/admin/login";
+import React from "react";
+
+function page() {
+  return (
+    <div className="flex justify-center items-center w-full h-[70vh]">
+      <SignupFormDemo />
+    </div>
+  );
+}
+
+export default page;
