0ef7293bb3d12e80f1dda9340af3065386911231
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2024-12-22 18:45:50 +0200

    Format code for improved readability in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,12 @@ import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-w
 
 export default function Home() {
   return (
-
-      <BackgroundBeamsWithCollision className="pt-10">
-        <div className="xl:w-3/4 w-full mx-auto p-5 flex flex-col gap-5">
-          <Latestblog />
-          <Mostpopular />
-          <ArticleTabs />
-        </div>
-      </BackgroundBeamsWithCollision>
- 
+    <BackgroundBeamsWithCollision className="pt-10">
+      <div className="xl:w-3/4 w-full mx-auto p-5 flex flex-col gap-5">
+        <Latestblog />
+        <Mostpopular />
+        <ArticleTabs />
+      </div>
+    </BackgroundBeamsWithCollision>
   );
 }
