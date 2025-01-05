7e1461ae1bcfd3b5b1b32b6b33f25e96842e3e41
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-01-05 18:58:21 +0200

    Fetch and display articles from API in ArticleLists component

--- src/components/articleLists.tsx
+++ src/components/articleLists.tsx
@@ -1,19 +1,60 @@
 "use client";
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { SkeletonCard } from "./blogSkeleton";
 import Article from "./article";
 
-function ArticleLists() {
-  // const [articles, setArticles] = useState([]);
+interface blog {
+  id: string;
+  coverImgUrl: string;
+  title: string;
+  description: string;
+  category: string;
+  createdAt: string;
+}
+
+function ArticleLists({ value }: { value: string }) {
+  const [articles, setArticles] = useState<blog[]>([]);
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          process.env.NEXT_PUBLIC_API_URL + "/blog",
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = response.data.filter((article: blog) => {
+          if (value === "all") {
+            return article;
+          }
+          return article.category === value;
+        });
+        setArticles(data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchArticles();
+  }, []);
 
   return (
     <div className="w-full h-full p-4">
       {loading ? (
         <SkeletonCard />
       ) : (
-        <div>
-          <Article />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-8">
+          {articles.map((article) => (
+            <Article key={article.id} blog={article} />
+          ))}
         </div>
       )}
     </div>
