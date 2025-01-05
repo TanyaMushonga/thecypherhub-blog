1abaa104c3f935041b89fcad313df3e4d531609a
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-01-05 18:58:14 +0200

    Refactor Article component to accept blog data as props and update rendering logic

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -5,17 +5,26 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import { BsDot } from "react-icons/bs";
 
-function Article() {
+interface Article {
+  id: string;
+  coverImgUrl: string;
+  title: string;
+  description: string;
+  category: string;
+  createdAt: string;
+}
+
+function Article({ blog }: { blog: Article }) {
   const router = useRouter();
   return (
     <div
-      className="flex flex-col md:w-1/4 w-full relative cursor-pointer"
+      className="flex flex-col w-full relative cursor-pointer"
       onClick={() => {
-        router.push("/read/1");
+        router.push(`/blog/${blog.id}`);
       }}
     >
       <Image
-        src="/blog_covers/iac.jpg"
+        src={blog?.coverImgUrl || '/blog_covers/codebuild.jpg'}
         width={340}
         height={200}
         alt="blog cover"
@@ -24,15 +33,15 @@ function Article() {
       />
       <div className="flex flex-col gap-2">
         <h1 className="text-white text-xl font-bold line-clamp-1 mt-1">
-          Infrastructure as code
+          {blog?.title}
         </h1>
-        <p className="text-slate-300 line-clamp-1">
-          #1: learn more - infrastructure as code concepts
-        </p>
+        <p className="text-slate-300 line-clamp-1">{blog?.description}</p>
         <div className="flex flex-row items-center gap-2">
           <p className="text-slate-300">4 min read</p>
           <BsDot className="text-slate-300" />
-          <p className="text-slate-300">{formatDate(new Date(2024, 11, 8))}</p>
+          <p className="text-slate-300">
+            {formatDate(new Date(blog?.createdAt))}
+          </p>
         </div>
       </div>
     </div>
