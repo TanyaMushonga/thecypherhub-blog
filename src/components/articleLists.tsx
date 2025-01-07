d3cc64d40da2d18dc148265f92242e263d347225
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-01-07 17:12:30 +0200

    Enhance ArticleLists component with loading state and Lottie animation for empty state

--- src/components/articleLists.tsx
+++ src/components/articleLists.tsx
@@ -1,6 +1,8 @@
 "use client";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import Lottie from "lottie-react";
+import empty from "@/lottie/empty.json";
 import { SkeletonCard } from "./blogSkeleton";
 import Article from "./article";
 
@@ -11,38 +13,39 @@ interface blog {
   description: string;
   category: string;
   createdAt: string;
+  content: string;
 }
 
 function ArticleLists({ value }: { value: string }) {
   const [articles, setArticles] = useState<blog[]>([]);
   const [loading, setLoading] = useState(false);
 
+  const fetchArticles = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        process.env.NEXT_PUBLIC_API_URL + "/blog",
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = response.data.filter((article: blog) => {
+        if (value === "all") {
+          return article;
+        }
+        return article.category === value;
+      });
+      setArticles(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
   useEffect(() => {
-    const fetchArticles = async () => {
-      try {
-        setLoading(true);
-        const response = await axios.get(
-          process.env.NEXT_PUBLIC_API_URL + "/blog",
-          {
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        const data = response.data.filter((article: blog) => {
-          if (value === "all") {
-            return article;
-          }
-          return article.category === value;
-        });
-        setArticles(data);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    };
     fetchArticles();
   }, []);
 
@@ -51,11 +54,31 @@ function ArticleLists({ value }: { value: string }) {
       {loading ? (
         <SkeletonCard />
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-8">
-          {articles.map((article) => (
-            <Article key={article.id} blog={article} />
-          ))}
-        </div>
+        <>
+          {articles.length > 0 ? (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-8">
+              {articles.map((article) => (
+                <Article key={article.id} blog={article} />
+              ))}
+            </div>
+          ) : (
+            <div className="w-full h-full flex flex-col gap-3 justify-center items-center">
+              <Lottie animationData={empty} loop={true} />
+              <h2 className="text-white font-bold text-xl md:text-2xl">
+                Something went wrong
+              </h2>
+              <p className="text-slate-300 text-lg ">
+                Seams like there is nothing here yet, try to refresh
+              </p>
+              <button
+                onClick={fetchArticles}
+                className="px-6 py-2 bg-transparent border border-blue-500 dark:border-white dark:text-white text-blue-500 rounded-lg font-bold transform hover:-translate-y-1 transition duration-400"
+              >
+                Refresh
+              </button>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
