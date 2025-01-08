1a7f1b96c1908948a38dbb9543682206fa211160
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-01-08 23:19:28 +0200

    Refactor LatestBlog component to use custom hook for article fetching and improve dynamic content rendering

--- src/components/latestblog.tsx
+++ src/components/latestblog.tsx
@@ -8,10 +8,12 @@ import { BsDot } from "react-icons/bs";
 import { useRouter } from "next/navigation";
 import LatestSkeleton from "./latestSkeleton";
 import MyProfile from "./myProfile";
+import { useFetchArticles } from "@/hooks/useFetchBlogs";
 
 function Latestblog() {
   const router = useRouter();
-  const [loading, setLoading] = useState(false);
+  const { articles, loading } = useFetchArticles("all");
+
   return (
     <>
       {loading ? (
@@ -20,23 +22,23 @@ function Latestblog() {
         <div
           className="flex md:flex-row flex-col gap-5 items-center pb-5 cursor-pointer"
           onClick={() => {
-            router.push("/read/1");
+            router.push(`/read/${articles[0]?.id}`);
           }}
         >
           <Image
-            src="/blog_covers/codebuild.jpg"
+            src={articles[0]?.coverImgUrl || "/blog_covers/codebuild.jpg"}
             width={600}
             height={200}
             alt="latest bog"
             className="rounded-md"
             priority
           />
-          <div className="flex flex-col md:text-center gap-2">
+          <div className="flex flex-col md:text-center gap-2 w-full">
             <h1 className="text-white md:text-3xl font-bold text-xl">
-              How whatsapp was able to scale to 50 bilion messages a day
+             {articles[0]?.title}
             </h1>
             <p className="text-slate-300 md:text-xl text-lg">
-              #1: learn more - whatsapp engineering concepts{" "}
+            {articles[0]?.description}
             </p>
             <div className="flex flex-row items-center gap-2 md:justify-center">
               {" "}
@@ -46,7 +48,7 @@ function Latestblog() {
             <div className="flex flex-row items-center md:justify-center">
               <LuPin className="text-slate-300 mr-2" />{" "}
               <p className="text-slate-300">
-                {formatDate(new Date(2024, 11, 2))}
+                {formatDate(new Date(articles[0]?.createdAt))}
               </p>
               <div className="flex flex-row items-center md:justify-center">
                 <BsDot className="text-slate-300" />
