650ff9a88dd4987e37f997def4d7813316aa498f
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-01-08 19:46:21 +0200

    Refactor useArticle store to support an array of blogs instead of a single blog object

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,24 +1,16 @@
 import { create } from "zustand";
 
-interface Article {
-  id: string;
-  coverImgUrl: string;
-  title: string;
-  description: string;
-  category: string;
-  createdAt: string;
-  content: string
-}
-
 export const useArticle = create<ArticleStore>((set) => ({
-  blog: {
-    id: "",
-    coverImgUrl: "",
-    title: "",
-    description: "",
-    category: "",
-    createdAt: "",
-    content: ""
-  },
-  setBlog: (blog: Article) => set({ blog }),
+  blog: [
+    {
+      id: "",
+      coverImgUrl: "",
+      title: "",
+      description: "",
+      category: "",
+      createdAt: "",
+      content: "",
+    },
+  ],
+  setBlog: (blog: Article[]) => set({ blog }),
 }));
