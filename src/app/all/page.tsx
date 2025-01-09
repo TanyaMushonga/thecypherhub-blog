a64990d6ee1dac18a52a261a3f1d84ef6f02fac7
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-01-09 09:42:05 +0200

    Integrate AllArticles component into page for enhanced article display

--- src/app/all/page.tsx
+++ src/app/all/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import AllArticles from "@/components/allArticles";
+import React from "react";
 
 function page() {
   return (
-    <div>page</div>
-  )
+    <div>
+      <AllArticles />
+    </div>
+  );
 }
 
-export default page
+export default page;
