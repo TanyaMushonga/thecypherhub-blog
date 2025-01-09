140d82922cefe66af4236a196ba20ad6f0d11870
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-01-09 15:15:57 +0200

    Add ParallaxScroll component for enhanced article display with scroll effects

---
+++ src/components/ui/parallax-scroll.tsx
@@ -0,0 +1,67 @@
+"use client";
+import { useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+import { cn } from "@/lib/utils";
+import Article from "../article";
+
+export const ParallaxScroll = ({
+  articles,
+  className,
+}: {
+  articles: Article[];
+  className?: string;
+}) => {
+  const gridRef = useRef<any>(null);
+  const { scrollYProgress } = useScroll({
+    container: gridRef, // remove this if your container is not fixed height
+    offset: ["start start", "end start"], // remove this if your container is not fixed height
+  });
+
+  const translateFirst = useTransform(scrollYProgress, [0, 1], [0, -200]);
+  const translateSecond = useTransform(scrollYProgress, [0, 1], [0, 200]);
+  const translateThird = useTransform(scrollYProgress, [0, 1], [0, -200]);
+
+  const third = Math.ceil(articles.length / 3);
+
+  const firstPart = articles.slice(0, third);
+  const secondPart = articles.slice(third, 2 * third);
+  const thirdPart = articles.slice(2 * third);
+
+  return (
+    <div
+      className={cn("h-[40rem] items-start overflow-y-auto w-full", className)}
+      ref={gridRef}
+    >
+      <div
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-start  max-w-5xl mx-auto gap-10 py-20 px-10"
+        ref={gridRef}
+      >
+        <div className="grid gap-10">
+          {firstPart.map((el, idx) => (
+            <motion.div
+              style={{ y: translateFirst }} // Apply the translateY motion value here
+              key={"grid-1" + idx}
+            >
+              <Article key={idx} blog={el} />
+            </motion.div>
+          ))}
+        </div>
+        <div className="grid gap-10">
+          {secondPart.map((el, idx) => (
+            <motion.div style={{ y: translateSecond }} key={"grid-2" + idx}>
+              <Article key={idx} blog={el} />
+            </motion.div>
+          ))}
+        </div>
+        <div className="grid gap-10">
+          {thirdPart.map((el, idx) => (
+            <motion.div style={{ y: translateThird }} key={"grid-3" + idx}>
+              <Article key={idx} blog={el} />
+            </motion.div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
