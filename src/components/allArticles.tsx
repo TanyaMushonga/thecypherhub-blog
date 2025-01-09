b399a34335a0cd4176f0a7ab9b1b9130e08096c4
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-01-09 09:42:23 +0200

    Add AllArticles component to display articles with parallax scrolling

---
+++ src/components/allArticles.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React from "react";
+import { ParallaxScroll } from "./ui/parallax-scroll";
+import { useFetchArticles } from "@/hooks/useFetchBlogs";
+
+function AllArticles() {
+  const { articles } = useFetchArticles("all");
+  return <ParallaxScroll articles={articles} />;
+}
+
+export default AllArticles;
