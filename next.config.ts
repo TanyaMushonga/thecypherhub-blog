bc378dac4bc3fe7602c67af9f6d3be2fd15d1d36
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-01-09 15:15:24 +0200

    Update image configuration to use remote patterns for S3 bucket

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["my-s3-bucket-for-files.s3.us-east-1.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "my-s3-bucket-for-files.s3.us-east-1.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
   },
   experimental: {
+    // Add any experimental features here
   },
 };
 
