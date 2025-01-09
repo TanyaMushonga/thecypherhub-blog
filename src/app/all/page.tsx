975f67a6863902d621eeff517668ed2bfe17525c
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-01-09 20:08:04 +0200

    Add metadata for page title in AllArticles component

--- src/app/all/page.tsx
+++ src/app/all/page.tsx
@@ -1,5 +1,11 @@
 import AllArticles from "@/components/allArticles";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Explore"
+}
+
 
 function page() {
   return (
