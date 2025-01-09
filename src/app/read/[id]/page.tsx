dc39d769ec90b101da9619dfaea6fd7372aa2b9e
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-01-09 20:08:10 +0200

    Add dynamic metadata generation for blog articles and improve static parameter fetching

--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -3,10 +3,65 @@ import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-w
 import SUbscribe from "@/components/Subscribe";
 import Related from "@/components/related";
 import Read from "@/components/read";
+import { Metadata } from "next";
+import axios from "axios";
 
-function page() {
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
 
+export async function generateStaticParams() {
+  const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/blog`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
+  const articles: Article[] = response?.data;
+
+  return articles.map(({ id }) => ({ params: { id } }));
+}
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const { id } = await params;
+
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/blog/${id}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const article = response?.data;
+
+    return {
+      title: article?.title,
+      description: article?.description,
+      openGraph: {
+        images: [
+          {
+            url: article?.coverImgUrl,
+            width: 800,
+            height: 600,
+            alt: article?.title,
+          },
+        ],
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching metadata:", error);
+    return {
+      title: "Error",
+      description: "An error occurred while fetching metadata.",
+    };
+  }
+}
+function Page() {
   return (
     <BackgroundBeamsWithCollision>
       <div className="xl:w-1/2 w-full mx-auto p-5 flex flex-col gap-5 mt-5">
@@ -18,4 +73,4 @@ function page() {
   );
 }
 
-export default page;
+export default Page;
