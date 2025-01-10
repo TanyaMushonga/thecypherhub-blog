5dccd95cec4d91f3b809543f7887a9e7e4954379
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-01-10 10:58:29 +0200

    Refactor CollisionMechanism to remove unused ref parameter for cleaner code

--- src/components/ui/background-beams-with-collision.tsx
+++ src/components/ui/background-beams-with-collision.tsx
@@ -114,7 +114,8 @@ const CollisionMechanism = React.forwardRef<
       repeatDelay?: number;
     };
   }
->(({ parentRef, containerRef, beamOptions = {} }, ref) => {
+
+>(({ parentRef, containerRef, beamOptions = {} }) => {
   const beamRef = useRef<HTMLDivElement>(null);
   const [collision, setCollision] = useState<{
     detected: boolean;
