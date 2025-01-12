534c356245e7c9842ccc73be1af5c077ee15de2d
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-01-12 20:15:43 +0200

    Refactor Home component layout and remove dynamic imports for better clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,24 @@
-import dynamic from "next/dynamic";
+// import ArticleTabs from "@/components/articleTabs";
+// import Latestblog from "@/components/latestblog";
+// import Mostpopular from "@/components/mostpopular";
+// import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
 
-const ArticleTabs = dynamic(() => import("@/components/articleTabs"), {
-  ssr: false,
-});
-const Latestblog = dynamic(() => import("@/components/latestblog"), {
-  ssr: false,
-});
-const Mostpopular = dynamic(() => import("@/components/mostpopular"), {
-  ssr: false,
-});
+// export default function Home() {
+//   return (
+//     <BackgroundBeamsWithCollision className="pt-10">
+//       <div className="xl:w-3/4 w-full mx-auto p-5 flex flex-col gap-5">
+//         <Latestblog />
+//         <Mostpopular />
+//         <ArticleTabs />
+//       </div>
+//     </BackgroundBeamsWithCollision>
+//   );
+// }
 
-export default function page() {
+export default function Home() {
   return (
-    <div className="pt-10">
-      <div className="xl:w-3/4 w-full mx-auto p-5 flex flex-col gap-5">
-        <Latestblog />
-        <Mostpopular />
-        <ArticleTabs />
-      </div>
+    <div className="container mx-auto">
+      <p className="text-white">Home</p>
     </div>
   );
 }
