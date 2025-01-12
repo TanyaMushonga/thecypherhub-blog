ab5a488b1930035caefbe157fef1c3394530d803
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-01-12 20:20:20 +0200

    Refactor Home component to include ArticleTabs, Latestblog, and Mostpopular for improved layout and structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-// import ArticleTabs from "@/components/articleTabs";
-// import Latestblog from "@/components/latestblog";
-// import Mostpopular from "@/components/mostpopular";
+import ArticleTabs from "@/components/articleTabs";
+import Latestblog from "@/components/latestblog";
+import Mostpopular from "@/components/mostpopular";
 // import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
 
 // export default function Home() {
@@ -17,8 +17,12 @@
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
-      <p className="text-white">Home</p>
+    <div className="pt-10">
+      <div className="xl:w-3/4 w-full mx-auto p-5 flex flex-col gap-5">
+        <Latestblog />
+        <Mostpopular />
+        <ArticleTabs />
+      </div>
     </div>
   );
 }
