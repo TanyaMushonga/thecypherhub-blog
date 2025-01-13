38b718d120e3b862ed65793875887d31954ce216
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-01-13 18:14:54 +0200

    Enhance NavBar with search functionality using dialog component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,61 +1,108 @@
 "use client";
-
+import { IoSearchOutline } from "react-icons/io5";
 import Link from "next/link";
 import React, { useState } from "react";
-import { FaBars, FaTimes, FaSearch } from "react-icons/fa";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import SearchList from "./SearchList";
+import { useFetchArticles } from "@/hooks/useFetchBlogs";
 
 function Navbar() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { articles } = useFetchArticles("all");
+  const [search, setSearch] = useState("");
 
-  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const filteredArticles = articles.filter(
+    (article) =>
+      article.title.toLowerCase().includes(search.toLowerCase()) ||
+      article.content.toLowerCase().includes(search.toLowerCase()) ||
+      article.description.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <nav className="fixed top-0 left-0 w-full bg-secondary backdrop-blur-md z-50">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        {/* Logo */}
         <div className="text-2xl font-bold">
           <Link className="text-white" href="/">
             The Dev Cycle
           </Link>
         </div>
-
-        {/* Search Field */}
-        <div className="hidden md:flex items-center">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="rounded-md px-4 py-2 text-gray-800 focus:outline-none"
-          />
-        </div>
+        <Dialog>
+          <DialogTrigger>
+            <div className="hidden md:flex items-center">
+              <input
+                type="text"
+                placeholder="Search..."
+                className="rounded-md px-4 py-2 text-white focus:outline-none bg-card"
+              />
+            </div>
+          </DialogTrigger>
+          <DialogContent className="bg-card">
+            <DialogHeader>
+              <div className="hidden">
+                <DialogTitle>Search</DialogTitle>
+              </div>
+              <div className="flex items-center">
+                <IoSearchOutline className="text-white w-5 h-5" />
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  className="rounded-md px-4 py-2 text-white focus:outline-none bg-card"
+                  onChange={(e) => setSearch(e.target.value)}
+                  value={search}
+                />
+              </div>
+            </DialogHeader>
+            <div className="flex flex-col gap-4">
+              {filteredArticles.length > 0 ? (
+                filteredArticles.map((article) => (
+                  <SearchList key={article.id} article={article} />
+                ))
+              ) : (
+                <p className="text-white">No results found</p>
+              )}
+            </div>
+          </DialogContent>
+        </Dialog>
 
         {/* Hamburger Menu */}
-        <button
-          className="md:hidden text-2xl focus:outline-none"
-          onClick={toggleMenu}
-        >
-          {isMenuOpen ? (
-            <FaTimes className="text-white" />
-          ) : (
-            <FaBars className="text-white" />
-          )}
-        </button>
-      </div>
-
-      {/* Mobile Menu */}
-      <div
-        className={`md:hidden transition-all duration-300 ${
-          isMenuOpen ? "block" : "hidden"
-        }`}
-      >
-        <div className="flex pb-4 px-5">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="rounded-l-md px-4 py-2 text-gray-800 flex-grow focus:outline-none"
-          />
-          <button className="bg-blue-600 hover:bg-blue-700 p-2 rounded-r-md">
-            <FaSearch />
-          </button>
+        <div className="md:hidden">
+          <Dialog>
+            <DialogTrigger>
+              <IoSearchOutline className="text-white w-7 h-7" />
+            </DialogTrigger>
+            <DialogContent className="bg-card">
+              <DialogHeader>
+                <div className="hidden">
+                  <DialogTitle>Search</DialogTitle>
+                </div>
+                <div className="flex items-center">
+                  <IoSearchOutline className="text-white w-5 h-5" />
+                  <input
+                    type="text"
+                    placeholder="Search..."
+                    className="rounded-md px-4 py-2 text-white focus:outline-none bg-card"
+                    onChange={(e) => setSearch(e.target.value)}
+                    value={search}
+                  />
+                </div>
+              </DialogHeader>
+              <div className="flex flex-col gap-4">
+                {filteredArticles.length > 0 ? (
+                  filteredArticles.map((article) => (
+                    <SearchList key={article.id} article={article} />
+                  ))
+                ) : (
+                  <p className="text-white">No results found</p>
+                )}
+              </div>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
     </nav>
