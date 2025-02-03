f805b9c4ff8004bf22ce672e05fb22fcc7038f58
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-02-03 19:12:29 +0200

    Remove duplicate import statement for Script in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import Script from "next/script";
-import Script from "next/script";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import { Inter } from "next/font/google";
