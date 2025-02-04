1507af719cffda4a6b3d3c1a771ee70369bfa42b
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-02-04 20:20:23 +0200

    Refactor page component to fetch articles and render with ParallaxScroll for enhanced user experience

--- src/app/all/page.tsx
+++ src/app/all/page.tsx
@@ -1,12 +1,8 @@
-import AllArticles from "@/components/common/allArticles";
+import { ParallaxScroll } from "@/components/ui/parallax-scroll";
 import React from "react";
 
-function page() {
-  return (
-    <div>
-      <AllArticles />
-    </div>
-  );
+export default async function page() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/blog`);
+  const data = await response.json();
+  return <ParallaxScroll articles={data} />;
 }
-
-export default page;
