9dfa6ddce000dd0e38d5b16b3b6c7e5a748242e8
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-02-04 20:15:20 +0200

    Refactor Mostpopular component to accept mostPopular prop for improved data handling

--- src/components/common/mostpopular.tsx
+++ src/components/common/mostpopular.tsx
@@ -5,12 +5,9 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import { LuPin } from "react-icons/lu";
 import { formatDate, getRandomArticles } from "@/lib/utils";
-import { useFetchArticles } from "@/hooks/useFetchBlogs";
-import MostPopularSkeleton from "./mostPopularSkeleton";
 
-function Mostpopular() {
+function Mostpopular({ mostPopular }: { mostPopular: Article[] }) {
   const router = useRouter();
-  const { loading, articles } = useFetchArticles("all");
   return (
     <div className="lg:flex flex-col hidden gap-4 mb-4">
       <div className="flex flex-row justify-between items-center">
@@ -20,43 +17,39 @@ function Mostpopular() {
         </Link>
       </div>
       <div className="flex flex-row gap-4">
-        {loading ? (
-          <MostPopularSkeleton />
-        ) : (
-          <>
-            {getRandomArticles(articles, 3).map((article) => (
-              <div
-                className="bg-card w-1/3 p-4 rounded-lg flex flex-row justify-between cursor-pointer gap-2"
-                key={article?.id}
-                onClick={() => {
-                  router.push(`/read/${article?.id}`);
-                }}
-              >
-                <div className="flex flex-col">
-                  <p className="text-white text-md font-bold line-clamp-2">
-                    {article?.title}
+        {getRandomArticles(mostPopular, 3).map((article) => (
+          <div
+            className="bg-card w-1/3 p-4 rounded-lg flex flex-row justify-between cursor-pointer gap-2"
+            key={article?.id}
+            onClick={() => {
+              router.push(`/read/${article?.id}`);
+            }}
+          >
+            <div className="flex flex-col">
+              <p className="text-white text-md font-bold line-clamp-2">
+                {article?.title}
+              </p>
+              <div className="flex flex-row items-center justify-between">
+                <div className="flex flex-row items-center">
+                  <LuPin className="text-slate-300 mr-2" />{" "}
+                  <p className="text-white">
+                    {formatDate(new Date(article?.createdAt))}
                   </p>
-                  <div className="flex flex-row items-center justify-between">
-                    <div className="flex flex-row items-center">
-                      <LuPin className="text-slate-300 mr-2" />{" "}
-                      <p className="text-white">
-                        {formatDate(new Date(article?.createdAt))}
-                      </p>
-                    </div>
-                  </div>
                 </div>
-                <Image
-                  src={article?.coverImgUrl || "/blog_covers/devops.webp"}
-                  width={120}
-                  height={50}
-                  alt="article cover"
-                  className="rounded-md"
-                  priority
-                />
               </div>
-            ))}
-          </>
-        )}
+            </div>
+            <Image
+              src={article?.coverImgUrl || ""}
+              width={120}
+              height={50}
+              alt="article cover"
+              className="rounded-md"
+              priority
+              loading="eager"
+              sizes="(max-width: 600px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
