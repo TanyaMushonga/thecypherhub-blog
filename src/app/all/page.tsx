6666d434072f47240e28161f76b5403f3db7f127
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-02-04 23:04:39 +0200

    Update data fetching in page component to use cache for improved performance

--- src/app/all/page.tsx
+++ src/app/all/page.tsx
@@ -2,7 +2,10 @@ import { ParallaxScroll } from "@/components/ui/parallax-scroll";
 import React from "react";
 
 export default async function page() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/blog`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/blog`, {
+    cache: 'force-cache'
+  });
   const data = await response.json();
+
   return <ParallaxScroll articles={data} />;
 }
