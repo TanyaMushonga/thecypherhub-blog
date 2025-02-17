751b432d22733870b2d680d59916f5225291158d
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-02-17 15:34:04 +0200

    Refactor Mostpopular component layout for improved responsiveness and structure

--- src/components/common/mostpopular.tsx
+++ src/components/common/mostpopular.tsx
@@ -9,17 +9,17 @@ import { formatDate, getRandomArticles } from "@/lib/utils";
 function Mostpopular({ mostPopular }: { mostPopular: Article[] }) {
   const router = useRouter();
   return (
-    <div className="lg:flex flex-col hidden gap-4 mb-4">
-      <div className="flex flex-row justify-between items-center">
+    <div className="lg:flex flex-col gap-4 mb-4">
+      <div className="flex flex-row justify-between items-center mb-4">
         <p className="text-white text-xl font-bold">Most Popular</p>
         <Link prefetch={false} className="text-white" href={"/all"}>
           VIEW ALL
         </Link>
       </div>
-      <div className="flex flex-row gap-4">
+      <div className="flex md:flex-row flex-col gap-4">
         {getRandomArticles(mostPopular, 3).map((article) => (
           <div
-            className="bg-card w-1/3 p-4 rounded-lg flex flex-row justify-between cursor-pointer gap-2"
+            className="bg-card w-full md:w-1/3 p-4 rounded-lg flex flex-row justify-between cursor-pointer gap-2"
             key={article?.id}
             onClick={() => {
               router.push(`/read/${article?.id}`);
