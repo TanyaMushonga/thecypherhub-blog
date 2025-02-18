6823b7f8b10a7753dd574ce5d3cef488b93a17fd
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-02-18 12:41:21 +0200

    Remove unused BackgroundBeamsWithCollision component and adjust padding for improved layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Latestblog from "@/components/common/latestblog";
 import LatestSkeleton from "@/components/common/latestSkeleton";
 import Mostpopular from "@/components/common/mostpopular";
 import MostPopularSkeleton from "@/components/common/mostPopularSkeleton";
-import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
 import { Suspense } from "react";
 
 export default async function Home() {
@@ -14,18 +13,16 @@ export default async function Home() {
   const latestBlog = articles[0];
 
   return (
-    <BackgroundBeamsWithCollision className="pt-10">
-      <div className="xl:w-3/4 w-full mx-auto p-5 flex flex-col gap-5">
-        <Suspense fallback={<LatestSkeleton />}>
-          <Latestblog latestBlog={latestBlog} />
-        </Suspense>
-        <Suspense fallback={<MostPopularSkeleton />}>
-          <Mostpopular mostPopular={articles} />
-        </Suspense>
-        <Suspense fallback={<BlogSkeleton />}>
-          <ArticleTabs allArticle={articles} />
-        </Suspense>
-      </div>
-    </BackgroundBeamsWithCollision>
+    <div className="xl:w-3/4 w-full mx-auto p-5 flex flex-col gap-5 pt-20">
+      <Suspense fallback={<LatestSkeleton />}>
+        <Latestblog latestBlog={latestBlog} />
+      </Suspense>
+      <Suspense fallback={<MostPopularSkeleton />}>
+        <Mostpopular mostPopular={articles} />
+      </Suspense>
+      <Suspense fallback={<BlogSkeleton />}>
+        <ArticleTabs allArticle={articles} />
+      </Suspense>
+    </div>
   );
 }
