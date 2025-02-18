8022e3737b024a86081358042e782e7b0028104c
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-02-18 12:42:45 +0200

    Refactor Mostpopular component to use RouteLayout for navigation, add MostPopularHeader, and optimize rendering with React.memo

--- src/components/common/mostpopular.tsx
+++ src/components/common/mostpopular.tsx
@@ -1,29 +1,20 @@
-"use client";
 import Image from "next/image";
-import Link from "next/link";
 import React from "react";
-import { useRouter } from "next/navigation";
 import { LuPin } from "react-icons/lu";
 import { formatDate, getRandomArticles } from "@/lib/utils";
+import MostPopularHeader from "./mostPopularHeader";
+import RouteLayout from "./routeLayout";
 
 function Mostpopular({ mostPopular }: { mostPopular: Article[] }) {
-  const router = useRouter();
   return (
     <div className="lg:flex flex-col gap-4 mb-4">
-      <div className="flex flex-row justify-between items-center mb-4">
-        <p className="text-white text-xl font-bold">Most Popular</p>
-        <Link prefetch={false} className="text-white" href={"/all"}>
-          VIEW ALL
-        </Link>
-      </div>
+      <MostPopularHeader />
       <div className="flex md:flex-row flex-col gap-4">
         {getRandomArticles(mostPopular, 3).map((article) => (
-          <div
+          <RouteLayout
             className="bg-card w-full md:w-1/3 p-4 rounded-lg flex flex-row justify-between cursor-pointer gap-2"
             key={article?.id}
-            onClick={() => {
-              router.push(`/read/${article?.id}`);
-            }}
+            link={`/read/${article?.id}`}
           >
             <div className="flex flex-col">
               <p className="text-white text-md font-bold line-clamp-2">
@@ -31,7 +22,7 @@ function Mostpopular({ mostPopular }: { mostPopular: Article[] }) {
               </p>
               <div className="flex flex-row items-center justify-between">
                 <div className="flex flex-row items-center">
-                  <LuPin className="text-slate-300 mr-2" />{" "}
+                  <LuPin className="text-slate-300 mr-2" />
                   <p className="text-white">
                     {formatDate(new Date(article?.createdAt))}
                   </p>
@@ -47,12 +38,14 @@ function Mostpopular({ mostPopular }: { mostPopular: Article[] }) {
               priority
               loading="eager"
               sizes="(max-width: 600px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              placeholder="blur"
+              blurDataURL="/path/to/placeholder-image.jpg" // Add a placeholder image
             />
-          </div>
+          </RouteLayout>
         ))}
       </div>
     </div>
   );
 }
 
-export default Mostpopular;
+export default React.memo(Mostpopular);
