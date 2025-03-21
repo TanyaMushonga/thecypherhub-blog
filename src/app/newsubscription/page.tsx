469497d480fcafabce93656eb2d206bdbf7570b2
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-03-21 14:57:01 +0200

    Add new subscription page component with basic layout

---
+++ src/app/newsubscription/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="xl:w-3/4 w-full mx-auto p-5 flex flex-col gap-5 pt-20 h-[82vh]">
+      page
+    </div>
+  );
+}
+
+export default page;
