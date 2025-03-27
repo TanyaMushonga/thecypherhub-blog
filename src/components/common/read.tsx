69b5d4bd8dc3304b3d934fedb9062921c9cb897d
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-03-27 16:23:31 +0200

    Enhance Read component with article metadata and add Subscribe and Disclaimer components

--- src/components/common/read.tsx
+++ src/components/common/read.tsx
@@ -5,6 +5,8 @@ import { LuPin } from "react-icons/lu";
 import { MdTimer } from "react-icons/md";
 import dynamic from "next/dynamic";
 import React from "react";
+import SUbscribe from "./Subscribe";
+import Disclaimer from "./disclaimer";
 
 const CopyLink = dynamic(() => import("@/components/common/copyLink"));
 
@@ -12,6 +14,40 @@ function Read({ article }: { article: Article }) {
   return (
     <div className="max-w-2xl mx-auto antialiased pt-4 relative">
       <div className="mb-10">
+        <h1 className="text-white md:text-3xl font-bold text-xl my-4">
+          <span className="text-blue-400 px-2">#</span>
+          {article?.title}
+        </h1>
+        <div className="flex md:flex-row flex-col md:items-center gap-2 mb-5 justify-between">
+          <div className="flex flex-row items-center gap-4">
+            <div className="flex flex-row items-center ">
+              <MdTimer className="text-slate-300 mr-2 text-lg" />
+              <p className="text-slate-300 text-lg">{article?.readTime}</p>
+            </div>
+            <div className="flex flex-row items-center">
+              <LuPin className="text-slate-300 mr-2 text-lg" />
+              <p className="text-slate-300 text-lg">
+                {article?.createdAt && formatDate(new Date(article.createdAt))}
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row items-center md:justify-center">
+            <Link
+              prefetch={false}
+              href={"https://tanyaradzwatmushonga.me"}
+              className="text-slate-300 cursor-pointer hover:underline mr-5 text-lg"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Written by <span className="text-blue-300">TanyaMushonga</span>
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 my-6">
+          <p className="text-slate-300 md:text-xl text-lg line-clamp-6">
+            {article?.description}
+          </p>
+        </div>
         <div className="text-sm dark:prose-invert text-white">
           {article?.coverImgUrl && (
             <Image
@@ -28,42 +64,17 @@ function Read({ article }: { article: Article }) {
               quality={100}
             />
           )}
-          <div className="flex flex-col gap-2">
-            <h1 className="text-white md:text-3xl font-bold text-xl">
-              {article?.title}
-            </h1>
-            <p className="text-slate-300 md:text-xl text-lg line-clamp-2">
-              {article?.description}
+          <div className="flex flex-row flex-wrap justify-between items-center border-b-2 py-3 border-slate-500 gap-4">
+            <p className="text-slate-300 text-lg">
+              Last updated on{" "}
+              {article?.updatedAt && formatDate(new Date(article.updatedAt))}
             </p>
-            <div className="flex md:flex-row flex-col md:items-center gap-2 mb-5 justify-between">
-              <div className="flex flex-row items-center gap-4">
-                <div className="flex flex-row items-center ">
-                  <MdTimer className="text-slate-300 mr-2 text-lg" />
-                  <p className="text-slate-300 text-lg">{article?.readTime}</p>
-                </div>
-                <div className="flex flex-row items-center">
-                  <LuPin className="text-slate-300 mr-2 text-lg" />
-                  <p className="text-slate-300 text-lg">
-                    {article?.createdAt &&
-                      formatDate(new Date(article.createdAt))}
-                  </p>
-                </div>
-              </div>
-              <div className="flex flex-row items-center md:justify-center">
-                <Link
-                  prefetch={false}
-                  href={"https://tanyaradzwatmushonga.me"}
-                  className="text-slate-300 cursor-pointer hover:underline mr-5 text-lg"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  @TanyaMushonga
-                </Link>
 
-                <CopyLink />
-              </div>
-            </div>
+            <CopyLink />
           </div>
+          <Disclaimer />
+          <SUbscribe />
+
           <div
             className="prose"
             dangerouslySetInnerHTML={{ __html: article?.content || "" }}
