74fcaa0c47bc7c711036846fa6ac48ff8434001f
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-03-28 22:12:56 +0200

    Refactor unsubscribe page to use Suspense for loading and integrate Unsubscribe component

--- src/app/unsubscribe/page.tsx
+++ src/app/unsubscribe/page.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import Unsubscribe from "@/components/common/unsubscribe";
+import React, { Suspense } from "react";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <div className="xl:w-3/4 w-full mx-auto p-5 flex items-center justify-center gap-5 pt-20 h-[82vh]">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Unsubscribe />
+      </Suspense>
+    </div>
+  );
 }
 
 export default page;
