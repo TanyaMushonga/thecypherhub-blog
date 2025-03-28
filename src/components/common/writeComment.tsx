6fc14f892e2411c25e19e36386e62233ebdb8bdf
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-03-28 13:47:33 +0200

    Add WriteAComment component for submitting comments

---
+++ src/components/common/writeComment.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import { MdSend } from "react-icons/md";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useTransition } from "react";
+import { writeComment } from "@/actions/comments";
+
+export const commentSchema = z.object({
+  comment: z.string().min(1),
+});
+
+export default function WriteAcomment() {
+  const [isPending, startTransition] = useTransition();
+  const form = useForm<z.infer<typeof commentSchema>>({
+    resolver: zodResolver(commentSchema),
+    defaultValues: {
+      comment: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof commentSchema>) {
+    try {
+      startTransition(() => {
+        writeComment("articleId", values).then((res) => {
+          if (res?.success) {
+            form.reset();
+            toast.success(res.success);
+          } else {
+            toast.error("Failed to submit the form. Please try again.");
+          }
+        });
+      });
+    } catch (error) {
+      console.error("Form submission error", error);
+      toast.error("Failed to submit the form. Please try again.");
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="mt-5">
+        <FormField
+          control={form.control}
+          name="comment"
+          render={({ field }) => (
+            <FormItem className="flex items-center gap-2 relative">
+              <FormControl>
+                <Input
+                  disabled={isPending}
+                  placeholder="Write a note"
+                  type="text"
+                  {...field}
+                  className="text-white bg-muted/30 border-none rounded-xl w-full"
+                />
+              </FormControl>
+              <Button
+                disabled={isPending}
+                type="submit"
+                className="text-foreground hover:bg-transparent hover:text-foreground absolute right-0 -top-2"
+                variant={"ghost"}
+              >
+                <MdSend className="w-8 h-8 text-white" />
+              </Button>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+}
