db8583baed6b57417caac4a11c4c36e2caa1e2e8
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-03-28 22:13:01 +0200

    Update CommentCard to format comment date using utility function

--- src/components/common/commentCard.tsx
+++ src/components/common/commentCard.tsx
@@ -1,16 +1,17 @@
+import { formatDate } from "@/lib/utils";
 import React from "react";
 
 function CommentCard({
   comment,
 }: {
-  comment: { id: string; comment: string, createdAt: string };
+  comment: { id: string; comment: string; createdAt: Date };
 }) {
   return (
     <div className="flex flex-col gap-2 mt-2 bg-card border-l-4 border-blue-300 p-2 rounded-lg  ">
       <p className="text-white ">{comment.comment}</p>
       <div className="flex flex-row justify-between mt-3">
         <p className="text-muted-foreground italic">- Anonymous</p>
-        <p className="text-slate-300">2 days ago</p>
+        <p className="text-slate-300">{formatDate(comment?.createdAt)}</p>
       </div>
     </div>
   );
