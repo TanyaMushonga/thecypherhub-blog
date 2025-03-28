647a4f626f0e5acc8be5f4800c5e18f4b163ef7a
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-03-28 13:47:09 +0200

    Add CommentCard component for displaying individual comments

---
+++ src/components/common/commentCard.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function CommentCard({
+  comment,
+}: {
+  comment: { id: string; comment: string, createdAt: string };
+}) {
+  return (
+    <div className="flex flex-col gap-2 mt-2 bg-card border-l-4 border-blue-300 p-2 rounded-lg  ">
+      <p className="text-white ">{comment.comment}</p>
+      <div className="flex flex-row justify-between mt-3">
+        <p className="text-muted-foreground italic">- Anonymous</p>
+        <p className="text-slate-300">2 days ago</p>
+      </div>
+    </div>
+  );
+}
+
+export default CommentCard;
