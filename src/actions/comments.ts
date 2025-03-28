009a577af7c566dcc4a5f23acd18355e4d718b69
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-03-28 14:38:00 +0200

    Refactor writeComment action to include validation for comment data using Zod

--- src/actions/comments.ts
+++ src/actions/comments.ts
@@ -1,37 +1,26 @@
 "use server";
 
-import { commentSchema } from "@/components/common/writeComment";
 import { revalidatePath } from "next/cache";
 
 import { z } from "zod";
-
-export const getComments = async (articleId: string) => {
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/comments?articleId=${articleId}`
-    );
-    if (!res.ok) {
-      return [];
-    }
-    const data = await res.json();
-    return data.comments;
-  } catch (error) {
-    console.error("Error fetching comments:", error);
-    return [];
-  }
-};
+import { commentSchema } from "../../schema/validations";
 
 export const writeComment = async (
   articleId: string,
   values: z.infer<typeof commentSchema>
 ) => {
   try {
+    const validatedValues = commentSchema.safeParse(values);
+    if (!validatedValues.success) {
+      return { error: "Invalid comment data" };
+    }
+    const { comment } = validatedValues.data;
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/comments`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ articleId, ...values }),
+      body: JSON.stringify({ articleId, comment }),
     });
     if (!res.ok) {
       return { error: "Failed to write comment" };
