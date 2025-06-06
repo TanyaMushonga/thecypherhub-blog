a4b5f5938b4ca9342a14bd3dc265ab72b219c1e5
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-06-06 15:54:51 +0200

    Remove unused Link component from MostPopularHeader

--- src/components/common/mostPopularHeader.tsx
+++ src/components/common/mostPopularHeader.tsx
@@ -1,13 +1,9 @@
-import Link from "next/link";
 import React from "react";
 
 function MostPopularHeader() {
   return (
     <div className="flex flex-row justify-between items-center mb-4">
       <p className="text-white text-xl font-bold">Most Popular</p>
-      <Link prefetch={false} className="text-white" href={"/all"}>
-        VIEW ALL
-      </Link>
     </div>
   );
 }
