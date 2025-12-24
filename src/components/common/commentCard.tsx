4b07cea2bbbc18691e03fe68d2d2d2d9e4bd62d7
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-12-24 18:59:49 +0200

    updated comment section

--- src/components/common/commentCard.tsx
+++ src/components/common/commentCard.tsx
@@ -7,10 +7,9 @@ function CommentCard({
   comment: { id: string; comment: string; createdAt: Date };
 }) {
   return (
-    <div className="flex flex-col gap-2 mt-2 bg-card border-l-4 border-blue-300 p-2 rounded-lg  ">
+    <div className="flex flex-col mt-2 bg-card p-2 rounded-lg  ">
       <p className="text-white ">{comment.comment}</p>
       <div className="flex flex-row justify-between mt-3">
-        <p className="text-muted-foreground italic">- Anonymous</p>
         <p className="text-slate-300">{formatDate(comment?.createdAt)}</p>
       </div>
     </div>
